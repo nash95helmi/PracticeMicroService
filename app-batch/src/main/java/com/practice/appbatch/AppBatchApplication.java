0734dea396943941ec641e1bb45ccaa40ec2065e
nash95 <nasaruddin.helmi@yahoo.com>
2021-07-03 10:31:57 +0800

    Add Batch Quartz

---
+++ app-batch/src/main/java/com/practice/appbatch/AppBatchApplication.java
@@ -0,0 +1,13 @@
+package com.practice.appbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBatchApplication.class, args);
+	}
+
+}
---
+++ app-batch/src/test/java/com/practice/appbatch/AppBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.appbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
